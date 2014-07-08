78277d8d0146204203eb8be3c877a53e57442311
Zach Greif <zsgreif@gmail.com>
2014-07-08 15:42:44 -0500

    Wrote put, successor, and add.

--- COMS228HW4/src/edu/iastate/cs228/hw4/OrderedMap.java
+++ COMS228HW4/src/edu/iastate/cs228/hw4/OrderedMap.java
@@ -34,26 +34,28 @@ public class OrderedMap<O extends Comparable<? super O>, M> implements OrderedMa
 
   public M get(Object orderingKey)
   {
-    
-    return null;
+    return map.get(orderingKey);
   }
-
+  
   public void put(O orderingKey, M mapValue)
   {
-    // TODO Auto-generated method stub
-
+    //Add the key to the BST
+    add(orderingKey);
+    
+    //Add the value to the map
+    map.put(orderingKey,  mapValue);
+    
+    size++; //increment size
   }
 
   public int size()
   {
-    // TODO Auto-generated method stub
-    return 0;
+    return size;
   }
 
   public boolean remove(Object orderingKey)
   {
-    // TODO Auto-generated method stub
-    return false;
+    
   }
 
   public Iterator<O> keyIterator()
@@ -98,4 +100,95 @@ public class OrderedMap<O extends Comparable<? super O>, M> implements OrderedMa
     return null;
   }
 
+  /**
+   * Adds a key into the Binary Search Tree.
+   * @param orderingKey - The key to be added to the Binary Search Tree.
+   * @throws IllegalArgumentException If orderingKey is already in the tree.
+   */
+  private void add(O orderingKey)
+  {
+    //We first check if root is null
+    BSTNode<O> current = root;
+    
+    //If root is not null, we have to find the correct position
+    while(current != null)
+    {
+      //Check if orderingKey is less than the current node's key
+      if(orderingKey.compareTo(current.data) < 0)
+      {
+        //If the current node has no left child, we set it's left child to a new node with orderingKey
+        if(current.leftChild == null)
+        {
+          current.leftChild = new BSTNode<O>(orderingKey, current, null, null);
+          return;
+        }
+        //If the current node has a left child, we make that our current node.
+        else
+        {
+          current = current.leftChild;
+        }
+      }
+    //Check if orderingKey is greater than the current node's key
+      else if(orderingKey.compareTo(current.data) > 0)
+      {
+      //If the current node has no right child, we set it's right child to a new node with orderingKey
+        if(current.rightChild == null)
+        {
+          current.rightChild = new BSTNode<O>(orderingKey, current, null, null);
+          return;
+        }
+      //If the current node has a right child, we make that our current node.
+        else
+        {
+          current = current.rightChild;
+        }
+      }
+      //Otherwise, (orderingKey is equal to the node's key) we throw an exception, because we don't allow duplicates
+      else
+      {
+        throw new IllegalArgumentException();
+      }
+    }
+    
+    //If root is null, the correct position is at the root
+    root = new BSTNode<O>(orderingKey);
+  }
+  
+  /**
+   * Determines the successor node of a given node.
+   * @param current - The node for which we want to find a successor.
+   * @return The node that succeeds the given node.
+   */
+  public BSTNode<O> successor(BSTNode<O> current)
+  {
+    //If there are no children, there is no successor.
+    if(current.leftChild == null && current.rightChild == null)
+      return null;
+    
+    //If left is not null and right is null, left is the successor.
+    else if(current.leftChild != null && current.rightChild == null)
+      return current.leftChild;
+    
+    //If right is not null and left is null, right is the successor.
+    else if(current.rightChild == null && current.rightChild != null)
+      return current.rightChild;
+    
+    //If there are two children, we return the node with the next greatest value
+    else
+    {
+      BSTNode<O> successor = current.rightChild;
+      while(successor != null)
+      {
+        if(successor.leftChild == null)
+        {
+          break;
+        }
+        else
+        {
+          successor = successor.leftChild;
+        }
+      }
+      return successor;
+    }
+  }
 }
